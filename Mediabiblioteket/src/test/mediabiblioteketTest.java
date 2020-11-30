ed41c8eeeab405e7e94019504584b3ae1b9a7f8a
Avacade2k <57358089+Avacade2k@users.noreply.github.com>
2020-11-30 16:07:14 +0100

    setup

---
+++ Mediabiblioteket/src/test/mediabiblioteketTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import mediabiblioteket.GUI;
+
+class mediabiblioteketTest {
+	
+	GUI gui;
+
+	@Test
+	void testGUI() {
+		gui = new GUI();
+		
+		fail("Not yet implemented");
+	}
+
+}
